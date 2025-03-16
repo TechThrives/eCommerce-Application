2400da6c9d72469868b9dc3b39af8e950a313afb
KanadeDevs <kanadebrothers1248@gmail.com>
2025-03-16 19:57:27 +0530

    fix: update cors urls

--- Backend/src/main/java/com/project/digitalshop/config/WebConfig.java
+++ Backend/src/main/java/com/project/digitalshop/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.project.digitalshop.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${base.url}")
+    private String baseURL;
+
+    @Value("${admin.base.url}")
+    private String adminBaseURL;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://thedigitalshop.vercel.app", "https://admin-thedigitalshop.vercel.app")
+                .allowedOrigins(baseURL, adminBaseURL)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
